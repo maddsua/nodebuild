a0e2e59ffd0022fe162b1cd78924873dd3f052ba
maddsua <102395937+maddsua@users.noreply.github.com>
2023-03-08 22:37:47 +0200

    Update path.ts

--- src/path.ts
+++ src/path.ts
@@ -9,6 +9,6 @@ export const noExtension = (path: string) => path.replace(/\..+$/, '');
 
 export const noFile = (path: string) => {
 	const slash = path.lastIndexOf('/');
-	if (slash < 0) return path;
+	if (slash < 0) return './';
 	return path.slice(0, slash)
 };
